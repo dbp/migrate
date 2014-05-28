b2bb8544724d8868aa8f1ce2c881dc94b625e659
Daniel Patterson <dbp@riseup.net>
2014-05-28 11:50:04 -0400

    very rough first cut

---
+++ src/Database/Migrate.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Database.Migrate where
+
+import Control.Applicative
+import Control.Monad (when)
+import Control.Monad.Reader
+import Control.Monad.Trans (lift, liftIO)
+import Data.Maybe (fromJust, fromMaybe)
+import Data.String (fromString)
+import Database.PostgreSQL.Simple
+import System.Posix.Env (getEnv)
+
+-- Helpers for running snaplets
+import Data.Map as M (empty)
+import Snap.Snaplet (Handler, SnapletInit)
+import Snap.Test (get)
+import Snap.Snaplet.Test (runHandler)
+
+data Env = Env { unConn :: Connection, unTable :: String }
+
+type MigrateT m a = ReaderT Env m a
+
+getEnvSettings :: IO Env
+getEnvSettings = do conn <- connectPostgreSQL ""
+                    t <- fromMaybe "migrations" <$> getEnv "PGTABLE"
+                    return (Env conn t)
+
+runMain :: MigrateT IO () -> IO ()
+runMain act = do e <- getEnvSettings
+                 runReaderT act e
+
+runMainSnap :: SnapletInit b b -> MigrateT (Handler b b) () -> IO ()
+runMainSnap app act = do e <- getEnvSettings
+                         runH (runReaderT act e) "devel"
+  where runH h env = do r <- runHandler (Just env) (get "" M.empty) h app
+                        case r of
+                          Left err -> error (show err)
+                          Right _ -> return ()
+
+
+-- NOTE(dbp 2014-05-27): Need to know when things were successful, to mark as migrated.
+up :: (Functor m, MonadIO m) => m Bool -> MigrateT m ()
+up migration =
+  do upMode <- liftIO $ (== Just "up") <$> getEnv "MIGRATION_MODE"
+     nm <- liftIO $ fromJust <$> getEnv "MIGRATION_NAME"
+     when upMode $
+       do s <- lift migration
+          if s then
+             do (Env c t) <- ask
+                void $ liftIO $ execute c (fromString $ "INSERT INTO " ++
+                                           t ++ " (name) values (?)") (Only nm)
+                liftIO $ putStrLn $ "Applied migration: " ++ nm
+            else liftIO $ putStrLn $ "Applying migration failed: " ++ nm
+
+
+down :: (Functor m, MonadIO m) => m Bool -> MigrateT m ()
+down migration =
+  do downMode <- liftIO $ (== Just "down") <$> getEnv "MIGRATION_MODE"
+     nm <- liftIO $ fromJust <$> getEnv "MIGRATION_NAME"
+     when downMode $
+       do s <- lift migration
+          if s then
+             do (Env c t) <- ask
+                void $ liftIO $ execute c (fromString $ "DELETE FROM " ++
+                                           t ++ " WHERE name = ?") (Only nm)
+                liftIO $ putStrLn $ "Reverted migration: " ++ nm
+            else liftIO $ putStrLn $ "Reverting migration failed: " ++ nm
---
+++ src/Main.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+-- Base types
+import Control.Applicative
+import Control.Monad (mapM_, filterM, void)
+import Data.List (sort, isSuffixOf)
+import Data.String (fromString)
+import Data.Maybe (fromJust)
+
+-- Commandline processing
+import System.Environment (getArgs)
+import System.Posix.Env (getEnv)
+
+-- Config file parsing
+import Data.Configurator (autoReload, autoConfig, Worth (Required), lookupDefault, require)
+
+-- Directory manipulations
+import System.Directory (getDirectoryContents)
+
+-- Running shell commands
+import System.Process (createProcess, shell, waitForProcess, CreateProcess(..))
+
+-- Database connections
+import Database.PostgreSQL.Simple
+
+-- Library code
+import Database.Migrate
+
+data DBType = PG
+
+main :: IO ()
+main = do (cfg, _) <- autoReload autoConfig [Required "devel.cfg"]
+          dir <- lookupDefault "migrations" cfg "migrate-directory"
+          table <- lookupDefault "migrations" cfg "migrate-table"
+          dbtype <- lookupDefault "postgresql" cfg "migrate-database-type" :: IO String
+          ghcargs <- lookupDefault "" cfg "migrate-ghc-args"
+          (conn, typ, env) <- case dbtype of
+                              "postgresql" ->
+                                do dbuser <- require cfg "migrate-database-user"
+                                   dbpass <- require cfg "migrate-database-password"
+                                   dbname <- require cfg "migrate-database-name"
+                                   dbhost <- lookupDefault "127.0.0.1" cfg "migrate-database-host"
+                                   dbport <- lookupDefault 5432 cfg "migrate-database-port"
+                                   c <- connect (ConnectInfo dbhost dbport dbuser dbpass dbname)
+                                   execute_ c (fromString createTable)
+                                   -- NOTE(dbp 2014-05-28): To appease cabal, so we can use sandboxes.
+                                   home <- fromJust <$> getEnv "HOME"
+                                   path <- fromJust <$> getEnv "PATH"
+                                   let env = [("PGHOST", dbhost)
+                                             ,("PGPORT", show dbport)
+                                             ,("PGDATABASE", dbname)
+                                             ,("PGUSER", dbuser)
+                                             ,("PGPASSWORD", dbpass)
+                                             ,("PGTABLE", table)
+                                             ,("HOME", home)
+                                             ,("PATH", path)]
+                                   return (c, PG, env)
+                                  where createTable = "CREATE TABLE IF NOT EXISTS " ++ table ++
+                                                      " (name text NOT NULL PRIMARY KEY)"
+          migrations <- sort . map stripSuffix . filter isCode <$> getDirectoryContents dir
+          args <- getArgs
+          case length args of
+            1 -> do let mode = head args
+                    case mode of
+                      "up" -> do
+                        missing <- filterM (notExists typ table conn) migrations
+                        if null missing
+                           then putStrLn "No migrations to run."
+                           else mapM_ (runMigration "up" ghcargs env dir) missing
+                      "down" -> do
+                        toDown <- dropWhileM (notExists typ table conn) $ reverse migrations
+                        case toDown of
+                          (x:xs) -> runMigration "down" ghcargs env dir x
+                          _ -> putStrLn "No migrations remaining."
+            _ -> putStrLn "usage: migrate [up|down]"
+
+  where stripSuffix = reverse . drop 3 . reverse
+        isCode = isSuffixOf ".hs"
+        notExists PG tb c m =
+          null <$> getMigration tb c m
+        getMigration :: String -> Connection -> String -> IO [(Only String)]
+        getMigration tb c m = query c (fromString $ "SELECT name FROM " ++ tb ++ " WHERE name = ?") (Only m)
+        dropWhileM :: Monad m => (a -> m Bool) -> [a] -> m [a]
+        dropWhileM f [] = return []
+        dropWhileM f (x:xs) = do r <- f x
+                                 if r
+                                    then dropWhileM f xs
+                                    else return (x:xs)
+
+runMigration :: String -> String -> [(String,String)] -> FilePath -> String -> IO ()
+runMigration mode ghcargs env dir p =
+  do (_,_,_,h) <- createProcess $ (shell ("/home/dbp/.cabal/bin/cabal exec runghc -- " ++ ghcargs ++ " " ++ dir ++ "/" ++ p ++ ".hs"))
+                                     { env = Just (env ++ [("MIGRATION_NAME", p)
+                                                          ,("MIGRATION_MODE", mode)])}
+     void $ waitForProcess h
